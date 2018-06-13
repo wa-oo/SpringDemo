cc0183b7eae1c4af68490a9fe869b488e385a96b
Nicelixiang <2570382927@qq.com>
2018-06-13 15:37:01 +0800

    李祥

--- src/main/java/cn/taiji/com/controller/OrgController.java
+++ src/main/java/cn/taiji/com/controller/OrgController.java
@@ -1,5 +1,13 @@
 package cn.taiji.com.controller;
 
-public class OrgController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class OrgController {
+	@RequestMapping("index")
+	public String Ssave() {
+		return "index";
+	}
 }
